85ddd55cdad275bb5e2a072d1492a3100f69b69e
George Gu <105337762+cga84@users.noreply.github.com>
2022-07-15 23:00:16 -0400

    login with existing user, right now directly go to home page

--- app/src/main/java/com/example/momento/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/momento/ui/login/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.momento.ui.login;
 
 import android.app.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -14,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -25,16 +27,26 @@ import android.widget.Toast;
 
 import com.example.momento.R;
 import com.example.momento.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
     private ActivityLoginBinding binding;
     private Button next;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "LoginActivity";
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //initializing need to be inside onCreate
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_login);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -122,11 +134,29 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-
-                loadingProgressBar.setVisibility(View.VISIBLE);
+//                all ignored
+//                loadingProgressBar.setVisibility(View.VISIBLE);
 
 //                loginViewModel.login(usernameEditText.getText().toString(),
 //                        passwordEditText.getText().toString());
+                mAuth.signInWithEmailAndPassword(usernameEditText.getText().toString(), passwordEditText.getText().toString())
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
+                            }
+                        });
             }
         });
         next =(Button) findViewById(R.id.next);
@@ -141,6 +171,27 @@ public class LoginActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private void updateUI(FirebaseUser currentUser){
+        //here to manage next move after successfully login
+        if(currentUser.getUid() == null){
+            //jump to registration page
+        }
+        //another condition to see which group of user the current user belongs, not sure how to get the data from server yet
+        openHome(); //testing
+
+    }
+
     public void openHome(){
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
@@ -154,4 +205,6 @@ public class LoginActivity extends AppCompatActivity {
     private void showLoginFailed(@StringRes Integer errorString) {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
     }
+
+    private void reload(){}
 }
