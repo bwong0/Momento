46e58ec2c6897f94caf3d6fad331e5d5bbb9ae68
billywonger@gmail.com <billywonger@gmail.com>
2022-07-19 17:23:56 -0700

    create and edit profiles

--- app/src/main/java/com/example/momento/family/ProfileCreation.java
+++ app/src/main/java/com/example/momento/family/ProfileCreation.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,8 +19,11 @@ import java.util.ArrayList;
 
 public class ProfileCreation extends AppCompatActivity implements Serializable {
     ArrayList<Persons> ArrayListProfiles;
-    TextView title;
+    EditText title;
+    EditText relationship;
     Persons profile;
+    Button update;
+    Button clear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,25 +31,84 @@ public class ProfileCreation extends AppCompatActivity implements Serializable {
 
 
         ImageView pictureProfileCreation = (ImageView) findViewById(R.id.profileCreationImage);
-        title = (TextView) findViewById(R.id.ProfileCreationTitle);
+        title = (EditText) findViewById(R.id.ProfileCreationTitle);
+        relationship = (EditText) findViewById(R.id.editRelationship);
+
         profile = (Persons) getIntent().getSerializableExtra("person");
 
-
-
-
+        update = (Button) findViewById((R.id.updateButton));
+        clear =(Button) findViewById(R.id.clearButton);
+        String uri = "@drawable/empty";
         if(profile.profilePresent == true){
             title.setText(profile.getName());
-            int profilePicture = getResources().getIdentifier(profile.getImage(),null,getPackageName());
-            Drawable res = getResources().getDrawable(profilePicture);
-            pictureProfileCreation.setImageDrawable(res);
+            if(profile.getImage() != ""){
+                int profilePicture = getResources().getIdentifier(profile.getImage(),null,getPackageName());
+                Drawable res = getResources().getDrawable(profilePicture);
+                pictureProfileCreation.setImageDrawable(res);
+            }
+            else{
+                int defaultImage = getResources().getIdentifier(uri, null, getPackageName());
+                Drawable res = getResources().getDrawable(defaultImage);
+                pictureProfileCreation.setImageDrawable(res);
+            }
         }
+
 
         else {
             title.setText("New Profile");
-            String uri = "@drawable/empty";
+
             int defaultImage = getResources().getIdentifier(uri, null, getPackageName());
             Drawable res = getResources().getDrawable(defaultImage);
             pictureProfileCreation.setImageDrawable(res);
         }
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean titleIS = true;
+                boolean relationshipIS = true;
+                if(title.getText().toString().trim() == "" || title.getText().toString() == "New Profile"){
+                    titleIS = false;
+                }
+
+                if(relationship.getText().toString().trim() == "" || relationship.getText().toString() == "Relationship"){
+                    titleIS = false;
+                }
+
+                if (titleIS == true && relationshipIS == true){
+                    profile.setName(title.getText().toString());
+                    profile.setRelationship(relationship.getText().toString());
+                    profile.setProfilePresent(true);
+                    update(profile);
+                }
+                else if(titleIS == false){
+                    title.setError("Please enter a name");
+                }
+                else if(relationshipIS == false){
+                    relationship.setError("Please enter a relationship");
+                }
+            }
+        });
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                profile = new Persons();
+                clear(profile);
+            }
+        });
+
+
+
+    }
+    public void update (Persons persons){
+
+        Intent intent = new Intent(this, ProfileCreation.class);
+        intent.putExtra("person", persons);
+        startActivity(intent);
+    }
+    public void clear(Persons persons){
+        Intent intent = new Intent(this, ProfileCreation.class);
+        intent.putExtra("person", persons);
+        startActivity(intent);
     }
 }
