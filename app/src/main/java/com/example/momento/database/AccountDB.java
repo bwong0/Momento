785f13a68b41d0dfa52fc294a286c01e12b7803f
Peter Fan <cfa60@sfu.ca>
2022-07-23 14:54:44 -0700

    Getters and Setters to Firebase

--- app/src/main/java/com/example/momento/database/AccountDB.java
+++ app/src/main/java/com/example/momento/database/AccountDB.java
@@ -1,7 +1,16 @@
 package com.example.momento.database;
 
+import android.util.Patterns;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.InvalidPropertiesFormatException;
+import java.util.regex.Pattern;
 
 public class AccountDB {
 
@@ -40,30 +49,139 @@ public class AccountDB {
         this.isActive = true;
         // Initialize database reference point
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        // TODO: toMap and push to mDatabase.child("Accounts")
     }
 
+
     /**
-     * Setter for accType.
+     * Get Account's UID, or primary key.
+     * @return String of UID.
+     */
+    public String getUid() { return this.uid; }
+    // No setter allowed for uid.
+
+    /**
+     * Get Account's user type: ADMIN, PATIENT, or FAMILY.
+     * @return Enum AccountType or null.
+     */
+    public AccountType getAccType() {
+        return AccountType.valueOf(this.accType);
+    }
+    /**
+     * Set Account's user type: ADMIN, PATIENT, or FAMILY.
+     * Data in the class is not updated until success callback.
      */
     public void setAccType(AccountType type) {
-        switch (type) {
-            case ADMIN:
-                this.accType = "ADMIN";
-                break;
-            case PATIENT:
-                this.accType = "PATIENT";
-                break;
-            case FAMILY:
-                this.accType = "FAMILY";
-                break;
+        mDatabase.child("Accounts").child(uid).child("accountType").setValue(type.toString())
+            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    AccountDB.this.accType = type.toString();
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Write failed
+                    // ...
+                }
+            });
+    }
+
+    public String getFirstName() { return this.firstName; }
+    public void setFirstName(String firstName) {
+        mDatabase.child("Accounts").child(uid).child("firstName").setValue(firstName)
+            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    AccountDB.this.firstName = firstName;
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Write failed
+                    // ...
+                }
+            });
+    }
+
+    public String getLastName() { return this.lastName; }
+    public void setLastName(String lastName) {
+        AccountDB.this.lastName = lastName;
+        mDatabase.child("Accounts").child(uid).child("lastName").setValue(lastName)
+            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    AccountDB.this.lastName = lastName;
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Write failed
+                    // ...
+                }
+            });
+    }
+
+    public String getEmail() { return this.email; }
+    public void setEmail(String email) throws InvalidPropertiesFormatException {
+        if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mDatabase.child("Accounts").child(uid).child("email").setValue(email)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        AccountDB.this.email = email;
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Write failed
+                        // ...
+                    }
+                });
+        } else {
+            throw new InvalidPropertiesFormatException("Invalid email address.");
         }
     }
 
-    /**
-     * Setter for isActive.
-     */
-    public void setIsActive(Boolean boo) {
-        this.isActive = boo;
+    public String getAddress() { return this.address; }
+    public void setAddress(String address) {
+        mDatabase.child("Accounts").child(uid).child("address").setValue(address)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        AccountDB.this.address = address;
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Write failed
+                        // ...
+                    }
+                });
     }
+
+    public Boolean getIsActive() { return this.isActive; }
+    public void setIsActive(Boolean boo) {
+        mDatabase.child("Accounts").child(uid).child("isActive").setValue(boo)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        AccountDB.this.isActive = boo;
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Write failed
+                        // ...
+                    }
+                });
+    }
+
 
 }
