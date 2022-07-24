180dfdbae84b62feb7b5f729b3c76e1306123563
Peter Fan <cfa60@sfu.ca>
2022-07-23 17:45:55 -0700

    Push class to Firebase in Constructor.

--- app/src/main/java/com/example/momento/database/AccountDB.java
+++ app/src/main/java/com/example/momento/database/AccountDB.java
@@ -9,12 +9,26 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
 import java.util.InvalidPropertiesFormatException;
-import java.util.regex.Pattern;
+import java.util.Map;
+
+/**
+ * The Structure on Firebase Database is
+ * Accounts : {
+ *     uid : {
+ *         firstName : String
+ *         lastName : String
+ *         email : String
+ *         address : String
+ *         isActive : Boolean
+ *     },
+ *     uid : { ... },
+ * }
+ */
 
 public class AccountDB {
 
-    // TODO: make getter/setter for each field. Updates database.
     private String uid;
     private String accType;
     private String firstName;
@@ -22,10 +36,12 @@ public class AccountDB {
     private String email;
     private String address;
     private Boolean isActive;
+
+    private final String ACCOUNT_NODE = "Accounts";
     private DatabaseReference mDatabase;
 
-    public AccountDB() {
-       // Default constructor required for calls to DataSnapshot.getValue(AccountDB.class)
+    private AccountDB() {
+        // Default constructor required for calls to DataSnapshot.getValue(AccountDB.class)
     }
 
     /**
@@ -41,17 +57,56 @@ public class AccountDB {
               String email, String address) {
         // Initialize class data members
         this.uid = uid;
-        setAccType(type);
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.address = address;
-        this.isActive = true;
+        // Organize the children nodes into a map
+        Map<String, Object> info = new HashMap<>();
+        info.put("accountType", type.toString());
+        info.put("firstName", firstName);
+        info.put("lastName", lastName);
+        info.put("email", email);
+        info.put("address", address);
+        info.put("isActive", (Boolean) true);
         // Initialize database reference point
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        // TODO: toMap and push to mDatabase.child("Accounts")
+        // push to mDatabase.child("Accounts")
+        mDatabase.child(ACCOUNT_NODE).child(this.uid).setValue(info)
+            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    // update Class members after success
+                    AccountDB.this.accType = type.toString();
+                    AccountDB.this.firstName = firstName;
+                    AccountDB.this.lastName = lastName;
+                    AccountDB.this.email = email;
+                    AccountDB.this.address = address;
+                    AccountDB.this.isActive = true;
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Write failed
+                    // ...
+                }
+            });
     }
 
+    /**
+     * Convert AccountDB class information into a HashMap<String, Object>
+     * @return Hashmap of all the data members that need to be on Firebase.
+     */
+    public Map<String, Object> toMap() {
+        Map<String, Object> info = new HashMap<>();
+        info.put("accountType", accType);
+        info.put("firstName", firstName);
+        info.put("lastName", lastName);
+        info.put("email", email);
+        info.put("address", address);
+        info.put("isActive", isActive);
+        return info;
+    }
+
+
+    /* Getters and Setters */
 
     /**
      * Get Account's UID, or primary key.
@@ -72,7 +127,7 @@ public class AccountDB {
      * Data in the class is not updated until success callback.
      */
     public void setAccType(AccountType type) {
-        mDatabase.child("Accounts").child(uid).child("accountType").setValue(type.toString())
+        mDatabase.child(ACCOUNT_NODE).child(uid).child("accountType").setValue(type.toString())
             .addOnSuccessListener(new OnSuccessListener<Void>() {
                 @Override
                 public void onSuccess(Void aVoid) {
@@ -90,7 +145,7 @@ public class AccountDB {
 
     public String getFirstName() { return this.firstName; }
     public void setFirstName(String firstName) {
-        mDatabase.child("Accounts").child(uid).child("firstName").setValue(firstName)
+        mDatabase.child(ACCOUNT_NODE).child(uid).child("firstName").setValue(firstName)
             .addOnSuccessListener(new OnSuccessListener<Void>() {
                 @Override
                 public void onSuccess(Void aVoid) {
@@ -109,7 +164,7 @@ public class AccountDB {
     public String getLastName() { return this.lastName; }
     public void setLastName(String lastName) {
         AccountDB.this.lastName = lastName;
-        mDatabase.child("Accounts").child(uid).child("lastName").setValue(lastName)
+        mDatabase.child(ACCOUNT_NODE).child(uid).child("lastName").setValue(lastName)
             .addOnSuccessListener(new OnSuccessListener<Void>() {
                 @Override
                 public void onSuccess(Void aVoid) {
@@ -128,7 +183,7 @@ public class AccountDB {
     public String getEmail() { return this.email; }
     public void setEmail(String email) throws InvalidPropertiesFormatException {
         if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            mDatabase.child("Accounts").child(uid).child("email").setValue(email)
+            mDatabase.child(ACCOUNT_NODE).child(uid).child("email").setValue(email)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
@@ -149,7 +204,7 @@ public class AccountDB {
 
     public String getAddress() { return this.address; }
     public void setAddress(String address) {
-        mDatabase.child("Accounts").child(uid).child("address").setValue(address)
+        mDatabase.child(ACCOUNT_NODE).child(uid).child("address").setValue(address)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
@@ -167,7 +222,7 @@ public class AccountDB {
 
     public Boolean getIsActive() { return this.isActive; }
     public void setIsActive(Boolean boo) {
-        mDatabase.child("Accounts").child(uid).child("isActive").setValue(boo)
+        mDatabase.child(ACCOUNT_NODE).child(uid).child("isActive").setValue(boo)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
