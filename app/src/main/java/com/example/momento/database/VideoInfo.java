79a7231f9f056f454aafa1da571be3e8f5029822
Peter Fan <cfa60@sfu.ca>
2022-07-24 00:34:34 -0700

    Video class for FamilyDB

---
+++ app/src/main/java/com/example/momento/database/VideoInfo.java
@@ -0,0 +1,31 @@
+package com.example.momento.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Video Class
+ */
+public class VideoInfo {
+
+    public String url;
+    private int playCount;
+
+    // Constructor
+    VideoInfo(String url) {
+        this.url = url;
+        playCount = 0;
+    }
+
+    public int getPlayCount() { return playCount; }
+    public void incrementPlayCount() { playCount++; }
+    public void resetPlayCount() { playCount = 0 ;}
+
+    public Map<String, Object> toMap() {
+        Map<String, Object> result = new HashMap<>();
+        result.put("url", this.url);
+        result.put("playCount", this.playCount);
+        return result;
+    }
+
+}
