b3a021f11cd3b21302fe8413d321d506434ec0ff
George Gu <105337762+cga84@users.noreply.github.com>
2022-07-31 11:39:23 -0400

    added patient register from admin homepage

--- app/src/main/java/com/example/momento/ui/login/adminHome.java
+++ app/src/main/java/com/example/momento/ui/login/adminHome.java
@@ -2,13 +2,37 @@ package com.example.momento.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.momento.R;
+import com.example.momento.databinding.ActivityAdminHomeBinding;
+import com.example.momento.databinding.ActivityPatientRegisterBinding;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class adminHome extends AppCompatActivity {
+    private ActivityAdminHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
+
+        binding = ActivityAdminHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        final Button createP = binding.patientRegister;
+
+        createP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPatientRegister();
+            }
+        });
+    }
+
+    private void openPatientRegister(){
+        Intent intent = new Intent(this, patientRegister.class);
+        startActivity(intent);
     }
 }
